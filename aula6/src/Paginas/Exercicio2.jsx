ffed94bc2c4236c74773b9a7116893e636dbcaa2
Martchellinho <marcelo.maximo24.03@gmail.com>
2025-09-04 09:10:25 -0300

    04

--- aula6/src/Paginas/Exercicio2.jsx
+++ aula6/src/Paginas/Exercicio2.jsx
@@ -1,9 +1,45 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {useState} from "react";
 
-export default function Exercicio2() {
-    return(
-        <div>
+export default function Exercicio2()
+  {
+    const [temp, setTemp] = useState(0);
+    const [resultado, setResultado] = useState('');
+  
 
-        </div>
+  function calcular() {
+    setResultado(
+      <div>
+        A temperatura em  Celsius {(Number(temp)-32)*5/9 } 
+
+      </div>
     );
+ }
+
+
+
+  return (
+    <div>
+      <h1>Exercicio 2</h1>
+
+      <div className="container">
+      <form onSubmit={e => e.preventDefault()}>
+        <p>
+            Digite a temperatura em Fahrenheit: <br />
+            <input type="number" value={temp} onChange={e => setTemp(e.target.value)} />
+          </p>
+          
+          <p>
+            <input type="button" value="Calcular" onClick={calcular} />
+          </p>
+        </form>
+        <p>
+          Resultado: {resultado}
+        </p>
+        <p>
+          <Link to="/">Voltar</Link>
+        </p>
+     </div>
+    </div>
+  )
 }
